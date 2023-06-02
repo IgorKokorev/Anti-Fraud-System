a8b1d74a3cb593de805b4f03f7ca15f5e01d0415
Igor Kokorev <ikokorev@mail.ru>
2023-06-02 15:39:14 +0200

    Stage4

--- Anti-Fraud System/task/src/antifraud/controllers/UserController.java
+++ Anti-Fraud System/task/src/antifraud/controllers/UserController.java
@@ -1,15 +1,15 @@
 package antifraud.controllers;
 
 import antifraud.DBtables.UserDB;
-import antifraud.DTO.UsernameStatus;
+import antifraud.DTO.*;
 import antifraud.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,5 +24,67 @@ public class UserController {
 
         userRepo.deleteById(usersDB.get(0).getId());
         return new UsernameStatus(username, "Deleted successfully!");
+    }
+
+    @GetMapping("/api/auth/list")
+    public ResponseEntity<List<UserToReturn>> returnUsersList() {
+        List<UserDB> usersDB = userRepo.findAllByOrderById();
+        List<UserToReturn> usersToReturn = new ArrayList<>();
+
+        for (UserDB user: usersDB) {
+            usersToReturn.add(new UserToReturn(user));
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(usersToReturn);
+    }
+
+    @PutMapping("/api/auth/role")
+    public ResponseEntity<UserToReturn> changeRole(@RequestBody UsernameRole usernameRole) {
+        if (usernameRole == null || usernameRole.getUsername() == null || usernameRole.getRole() == null)
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Incomplete data");
+
+        List<UserDB> usersDB = userRepo.findByUsernameIgnoreCase(usernameRole.getUsername());
+        if (usersDB.size() == 0) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+
+        if (!usernameRole.getRole().equals("SUPPORT") && !usernameRole.getRole().equals("MERCHANT"))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Incorrect role");
+
+        UserDB userDB = usersDB.get(0);
+        if (userDB.getRole().equals(usernameRole.getRole()))
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Role already assigned");
+
+        userDB.setRole(usernameRole.getRole());
+        userDB = userRepo.save(userDB);
+
+        // returning user data
+        UserToReturn userToReturn = new UserToReturn(userDB);
+        return ResponseEntity.status(HttpStatus.OK).body(userToReturn);
+    }
+
+    @PutMapping("/api/auth/access")
+    public ResponseEntity<Status> changeRole(@RequestBody UsernameOperation usernameOperation) {
+        if (usernameOperation == null || usernameOperation.getUsername() == null || usernameOperation.getOperation() == null)
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Incomplete data");
+
+        List<UserDB> usersDB = userRepo.findByUsernameIgnoreCase(usernameOperation.getUsername());
+        if (usersDB.size() == 0) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+
+        if (!usernameOperation.getOperation().equals("LOCK") && !usernameOperation.getOperation().equals("UNLOCK"))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Incorrect operation");
+
+        UserDB userDB = usersDB.get(0);
+        boolean toUnlock = usernameOperation.getOperation().equals("UNLOCK");
+
+        if (userDB.getRole().equals("ADMINISTRATOR") && !toUnlock)
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Administrator can't be locked");
+
+        userDB.setAccountNonLocked(toUnlock);
+        userRepo.save(userDB);
+
+        // saving user data
+        UserToReturn userToReturn = new UserToReturn(userDB);
+
+        String status = "User " + userDB.getUsername() + " " + (toUnlock ? "un" : "") + "locked!";
+        return ResponseEntity.status(HttpStatus.OK).body(new Status(status));
     }
 }
